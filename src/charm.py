d227462492dc3ab684fcb709a451d0fe2c476afc
Luca Bello <luca.bello@canonical.com>
2025-02-05 14:39:52 +0100

    add pebble checks

--- src/charm.py
+++ src/charm.py
@@ -3,11 +3,17 @@
 # See LICENSE file for licensing details.
 """A Juju charm for OpenTelemetry Collector on Kubernetes."""
 
+import socket
 import os
+from typing import Any, Dict, Set
 
 from ops import CharmBase, main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, Port
 from ops.pebble import Layer
+
+PORTS: Set[Port] = {
+    Port(protocol="tcp", port=8888),  # for self-monitoring metrics
+}
 
 
 class OpenTelemetryCollectorK8sCharm(CharmBase):
@@ -18,11 +24,17 @@ class OpenTelemetryCollectorK8sCharm(CharmBase):
 
         self._name = "opentelemetry-collector"
         self._container = self.unit.get_container(self._name)
+        self._set_ports(ports=PORTS)
         self.reconcile()
 
     def reconcile(self):
         """Recreate the world state for the charm."""
+        if not self._container.can_connect():
+            # TODO:: set MaintenceStatus ?
+            return
+
         self._container.add_layer(self._name, self._pebble_layer, combine=True)
+        self._container.replan()
         self.unit.status = ActiveStatus()
 
     @property
@@ -49,6 +61,40 @@ class OpenTelemetryCollectorK8sCharm(CharmBase):
         )
 
         return layer
+
+    @property
+    def _pebble_checks(self) -> Dict[str, Any]:
+        """Pebble checks to run in the charm."""
+        checks = {
+            "health": {
+                "override": "replace",
+                "level": "alive",
+                "period": "30s",
+                "http": {"url": f"http://{socket.getfqdn()}:13133/health"},
+            },
+            "metrics": {
+                "override": "replace",
+                "level": "health",
+                "period": "30s",
+                "http": {"url": f"http://{socket.getfqdn()}:8888/metrics"},
+            },
+        }
+        return checks
+
+    def _set_ports(self, ports: Set[Port]) -> None:
+        """Open necessary (and close no longer needed) workload ports."""
+        if not self.unit.is_leader():
+            return
+        actual_ports = self.unit.opened_ports()
+
+        # Ports may change across an upgrade, so need to sync
+        ports_to_close = actual_ports.difference(ports)
+        for p in ports_to_close:
+            self.unit.close_port(p.protocol, p.port)
+
+        new_ports_to_open = ports.difference(ports)
+        for p in new_ports_to_open:
+            self.unit.open_port(p.protocol, p.port)
 
 
 if __name__ == "__main__":
