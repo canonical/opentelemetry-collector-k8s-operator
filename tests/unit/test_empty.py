0cd0ea97fea9e400b1af96c9f8c76787f48c6df4
Luca Bello <luca.bello@canonical.com>
2025-02-05 14:00:24 +0100

    add empty test

---
+++ tests/unit/test_empty.py
@@ -0,0 +1,9 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+# Learn more about testing at: https://juju.is/docs/sdk/testing
+
+
+def test_empty():
+    """Empty test."""
+    assert True
