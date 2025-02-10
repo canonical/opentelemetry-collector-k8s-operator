2079d47258f3f631d7c61a2a3fd9fe98b5067f4f
Luca Bello <luca.bello@canonical.com>
2025-02-10 14:02:36 +0100

    pr comments and other changes

--- src/charm.py
+++ src/charm.py
@@ -13,34 +13,28 @@ from ops import CharmBase, main
 from ops.model import ActiveStatus, Port
 from ops.pebble import Layer
 
-PORTS: Set[Port] = {
-    Port(protocol="tcp", port=8888),  # for self-monitoring metrics
-}
-
 
 class OpenTelemetryCollectorK8sCharm(CharmBase):
     """Charm to run OpenTelemetry Collector on Kubernetes."""
 
     def __init__(self, *args):
         super().__init__(*args)
-
-        self._name = "opentelemetry-collector"
-        self._container = self.unit.get_container(self._name)
-        self._config_manager = OpenTelemetryCollectorConfig()
+        if not self.unit.get_container("opentelemetry-collector").can_connect():
+            return
         self.reconcile()
 
     def reconcile(self):
         """Recreate the world state for the charm."""
-        if not self._container.can_connect():
-            # TODO:: set MaintenceStatus ?
-            return
+        name = "opentelemetry-collector"
+        container = self.unit.get_container(name)
+        config_manager = OpenTelemetryCollectorConfig()
 
-        self._set_ports(ports=PORTS)
+        self.unit.set_ports(*config_manager.ports)
 
-        self._container.push("/etc/otelcol/config.yaml", self._config_manager.build_config())
+        container.push("/etc/otelcol/config.yaml", config_manager.build_config())
 
-        self._container.add_layer(self._name, self._pebble_layer, combine=True)
-        self._container.replan()
+        container.add_layer(name, self._pebble_layer, combine=True)
+        container.replan()
 
         self.unit.status = ActiveStatus()
 
@@ -78,25 +72,10 @@ class OpenTelemetryCollectorK8sCharm(CharmBase):
                 "override": "replace",
                 "level": "alive",
                 "period": "30s",
-                "http": {"url": f"http://{socket.getfqdn()}:13133/health"},
+                "http": {"url": "http://localhost:13133/health"},
             },
         }
         return checks
-
-    def _set_ports(self, ports: Set[Port]) -> None:
-        """Open necessary (and close no longer needed) workload ports."""
-        if not self.unit.is_leader():
-            return
-        actual_ports = self.unit.opened_ports()
-
-        # Ports may change across an upgrade, so need to sync
-        ports_to_close = actual_ports.difference(ports)
-        for p in ports_to_close:
-            self.unit.close_port(p.protocol, p.port)
-
-        new_ports_to_open = ports.difference(ports)
-        for p in new_ports_to_open:
-            self.unit.open_port(p.protocol, p.port)
 
 
 if __name__ == "__main__":
--- src/config.py
+++ src/config.py
@@ -1,84 +1,80 @@
 """Helper module to build the configuration for OpenTelemetry Collector."""
 
+from typing import List
+
+import yaml
+
 
 class OpenTelemetryCollectorConfig:
-    """class."""  # TODO: add docstring
-
-    config = """
-# To limit exposure to denial of service attacks, change the host in endpoints below from 0.0.0.0 to a specific network interface.
-# See https://github.com/open-telemetry/opentelemetry-collector/blob/main/docs/security-best-practices.md#safeguards-against-denial-of-service-attacks
-
-extensions:
-  health_check:
-    endpoint: 0.0.0.0:13133
-  pprof:
-    endpoint: 0.0.0.0:1777
-  zpages:
-    endpoint: 0.0.0.0:55679
-
-receivers:
-  otlp:
-    protocols:
-      grpc:
-        endpoint: 0.0.0.0:4317
-      http:
-        endpoint: 0.0.0.0:4318
-
-  opencensus:
-    endpoint: 0.0.0.0:55678
-
-  # Collect own metrics
-  prometheus:
-    config:
-      scrape_configs:
-      - job_name: 'otel-collector'
-        scrape_interval: 10s
-        static_configs:
-        - targets: ['0.0.0.0:8888']
-
-  jaeger:
-    protocols:
-      grpc:
-        endpoint: 0.0.0.0:14250
-      thrift_binary:
-        endpoint: 0.0.0.0:6832
-      thrift_compact:
-        endpoint: 0.0.0.0:6831
-      thrift_http:
-        endpoint: 0.0.0.0:14268
-
-  zipkin:
-    endpoint: 0.0.0.0:9411
-
-processors:
-  batch:
-
-exporters:
-  debug:
-    verbosity: detailed
-
-service:
-
-  pipelines:
-
-    traces:
-      receivers: [otlp, opencensus, jaeger, zipkin]
-      processors: [batch]
-      exporters: [debug]
-
-    metrics:
-      receivers: [otlp, opencensus, prometheus]
-      processors: [batch]
-      exporters: [debug]
-
-    logs:
-      receivers: [otlp]
-      processors: [batch]
-      exporters: [debug]
-
-  extensions: [health_check, pprof, zpages]
-    """
+    """Configuration manager for OpenTelemetry Collector."""
 
     def build_config(self) -> str:
         """String."""
-        return self.config
+        config = {
+            "extensions": {
+                "health_check": {"endpoint": "0.0.0.0:13133"},
+                "pprof": {"endpoint": "0.0.0.0:1777"},
+                "zpages": {"endpoint": "0.0.0.0:55679"},
+            },
+            "receivers": {
+                "otlp": {
+                    "protocols": {
+                        "grpc": {"endpoint": "0.0.0.0:4317"},
+                        "http": {"endpoint": "0.0.0.0:4318"},
+                    }
+                },
+                "opencensus": {"endpoint": "0.0.0.0:55678"},
+                "prometheus": {
+                    "config": {
+                        "scrape_configs": [
+                            {
+                                "job_name": "otel-collector",
+                                "scrape_interval": "1m",
+                                "static_configs": [{"targets": ["0.0.0.0:8888"]}],
+                            }
+                        ]
+                    }
+                },
+                "jaeger": {
+                    "protocols": {
+                        "grpc": {"endpoint": "0.0.0.0:14250"},
+                        "thrift_binary": {"endpoint": "0.0.0.0:6832"},
+                        "thrift_compact": {"endpoint": "0.0.0.06831"},
+                        "thrift_http": {"endpoint": "0.0.0.0:14268"},
+                    }
+                },
+                "zipkin": {"endpoint": "0.0.0.0:9411"},
+            },
+            "processors": {"batch:": None},
+            "exporters": {"debug": {"verbosity": "detailed"}},
+            "service": {
+                "pipelines": {
+                    "traces": {
+                        "receivers": ["otlp", "opencensus", "jaeger", "zipkin"],
+                        "processors": ["batch"],
+                        "exporters": ["debug"],
+                    },
+                    "metrics": {
+                        "receivers": ["otlp", "opencensus", "prometheus"],
+                        "processors": ["batch"],
+                        "exporters": ["debug"],
+                    },
+                    "logs": {
+                        "receivers": ["otlp"],
+                        "processors": ["batch"],
+                        "exporters": ["debug"],
+                    },
+                },
+                "extensions": ["health_check", "pprof", "zpages"],
+            },
+        }
+        return yaml.dump(config)
+
+    @property
+    def ports(self) -> List[int]:
+        """Return the ports that are used in the Collector config."""
+        ports = [
+            8888,  # self-monitoring metrics,
+            13133,  # health check
+        ]
+        return ports
