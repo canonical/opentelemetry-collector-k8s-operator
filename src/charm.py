7680e4d753070e876a79b00204d87ea5d24ca55a
Michael Thamm <mike.thamm@canonical.com>
2025-02-24 16:27:00 -0500

    test

--- src/charm.py
+++ src/charm.py
@@ -8,13 +8,15 @@ from typing import Any, Dict
 from pathlib import Path
 import yaml
 import logging
+import socket
 
+from urllib.parse import urlparse
 from config import Config, Ports
 
 from ops import CharmBase, main
 from ops.model import ActiveStatus
 from ops.pebble import Layer
-from charms.loki_k8s.v1.loki_push_api import LogForwarder
+from charms.loki_k8s.v1.loki_push_api import LokiPushApiProvider
 from charms.prometheus_k8s.v0.prometheus_scrape import (
     MetricsEndpointConsumer,
 )
@@ -29,12 +31,13 @@ logger = logging.getLogger(__name__)
 RulesMapping = namedtuple("RulesMapping", ["src", "dest"])
 
 
-def _aggregate_alerts(self, alerts: Dict, rule_path_map: RulesMapping):
-    if os.path.exists(rule_path_map.dest):
-        shutil.rmtree(rule_path_map.dest)
-    shutil.copytree(rule_path_map.src, rule_path_map.dest)
+def _aggregate_alerts(alerts: Dict, rule_path_mapping: RulesMapping):
+    # TODO Do we need to make this extendable for loki_alerts like in grafana_agent.py #L493
+    if os.path.exists(rule_path_mapping.dest):
+        shutil.rmtree(rule_path_mapping.dest)
+    shutil.copytree(rule_path_mapping.src, rule_path_mapping.dest)
     for topology_identifier, rule in alerts.items():
-        rule_file = Path(rule_path_map.dest) / f"juju_{topology_identifier}.rules"
+        rule_file = Path(rule_path_mapping.dest) / f"juju_{topology_identifier}.rules"
         rule_file.write_text(yaml.safe_dump(rule))
         logger.debug(f"updated alert rules file {rule_file.as_posix()}")
 
@@ -44,8 +47,10 @@ class OpenTelemetryCollectorK8sCharm(CharmBase):
 
     _config_path = "/etc/otelcol/config.yaml"
     _container_name = "otelcol"
-    _rules_source_path = "src/prometheus_alert_rules"
-    _rules_destination_path = "prometheus_alert_rules"
+    _metrics_rules_src_path = "src/prometheus_alert_rules"
+    _metrics_rules_dest_path = "prometheus_alert_rules"
+    _loki_rules_src_path = "src/loki_alert_rules"
+    _loki_rules_dest_path = "loki_alert_rules"
 
     def __init__(self, *args):
         super().__init__(*args)
@@ -54,17 +59,29 @@ class OpenTelemetryCollectorK8sCharm(CharmBase):
 
         self.topology = JujuTopology.from_charm(self)
         self.otel_config = Config.default_config()
+        charm_root = self.charm_dir.absolute()
 
         # Logs setup
-        self._log_forwarder = LogForwarder(self, relation_name=self._endpoints["logging"])
+        self.loki_rules_paths = RulesMapping(
+            src=charm_root.joinpath(*self._loki_rules_src_path.split("/")),
+            dest=charm_root.joinpath(*self._loki_rules_dest_path.split("/")),
+        )
+        # TODO Determine if we can use these libs without events
+        internal_url = urlparse(self.internal_url)
+        self.loki_provider = LokiPushApiProvider(
+            self,
+            address=internal_url.hostname,
+            port=Ports.LOKI_GRPC.value,
+            scheme=internal_url.scheme,
+            path=internal_url.path,  # TODO "/loki/api/v1/push" is now pathless in otelcol
+        )
 
         # Metrics setup
-        charm_root = self.charm_dir.absolute()
-        self.metrics_consumer = MetricsEndpointConsumer(self)
         self.metrics_rules_paths = RulesMapping(
-            src=charm_root.joinpath(*self._rules_source_path.split("/")),
-            dest=charm_root.joinpath(*self._rules_destination_path.split("/")),
+            src=charm_root.joinpath(*self._metrics_rules_src_path.split("/")),
+            dest=charm_root.joinpath(*self._metrics_rules_dest_path.split("/")),
         )
+        self.metrics_consumer = MetricsEndpointConsumer(self)
         self.remote_write = PrometheusRemoteWriteConsumer(
             self, alert_rules_path=self.metrics_rules_paths.dest
         )
@@ -75,10 +92,11 @@ class OpenTelemetryCollectorK8sCharm(CharmBase):
         """Recreate the world state for the charm."""
         container = self.unit.get_container(self._container_name)
 
+        self._add_log_ingestion()
         self._add_remote_write()
         self._add_self_scrape()
         # Receive and update alert rules
-        _aggregate_alerts(self.metrics_consumer.alerts)
+        _aggregate_alerts(self.metrics_consumer.alerts, self.metrics_rules_paths)
         self.remote_write.reload_alerts()
         # Receive scrape jobs and add them to the otel config
         self.otel_config.add_prometheus_scrape(self.metrics_consumer.jobs())
@@ -90,6 +108,12 @@ class OpenTelemetryCollectorK8sCharm(CharmBase):
 
         self.unit.set_ports(*self.otel_config.ports)
         self.unit.status = ActiveStatus()
+
+    @property
+    def internal_url(self):
+        """Fqdn plus appropriate scheme and server port."""
+        scheme = "http"
+        return f"{scheme}://{socket.getfqdn()}"
 
     @property
     def _pebble_layer(self) -> Layer:
@@ -174,7 +198,7 @@ class OpenTelemetryCollectorK8sCharm(CharmBase):
         # TODO Receive alert rules via remote write
         # https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/37277
 
-    def _add_log_forwarding(self):
+    def _add_log_ingestion(self):
         # The Loki receiver implements the Loki push api.
         # It allows Promtail instances to specify the open telemetry collector as their lokiAddress.
         self.otel_config.add_receiver(
