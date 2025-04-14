7051d61e60aa103a60a2676853f4980776782f05
Michael Thamm <mike.thamm@canonical.com>
2025-04-14 19:31:22 +0100

    feat: Support dashboards (#21)
    * Enable forwarding bundled and related dashboards to Grafana
    * Fixed and new alert rules
    
    ---------
    
    Co-authored-by: Luca Bello <luca.bello@canonical.com>
    Co-authored-by: Leon <82407168+sed-i@users.noreply.github.com>

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+from shutil import copytree
 
 import pytest
 from ops.testing import Context, Exec
@@ -6,8 +8,15 @@ from charm import OpenTelemetryCollectorK8sCharm
 
 
 @pytest.fixture
-def ctx():
-    yield Context(OpenTelemetryCollectorK8sCharm)
+def ctx(tmp_path):
+    src_dirs = ["grafana_dashboards", "loki_alert_rules", "prometheus_alert_rules"]
+    # Create a virtual charm_root so Scenario respects the `src_dirs`
+    # Related to https://github.com/canonical/operator/issues/1673
+    for src_dir in src_dirs:
+        source_path = Path('src') / src_dir
+        target_path = tmp_path / "src" / src_dir
+        copytree(source_path, target_path, dirs_exist_ok=True)
+    yield Context(OpenTelemetryCollectorK8sCharm, charm_root=tmp_path)
 
 
 @pytest.fixture
