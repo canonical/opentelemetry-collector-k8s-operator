6f3b6badf9297a8943693c8c04bc7b17ff6bff0b
Michael Thamm <mike.thamm@canonical.com>
2025-04-17 20:51:20 +0100

    fix: housekeeping (#39)
    * Add unit test for add_prometheus_scrape
    * Support for jubilant `--keep-models` in CI

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -84,6 +84,60 @@ def test_add_telemetry():
     assert cfg._config["service"]["telemetry"]["logs"] == {"level": "INFO"}
 
 
+def test_add_prometheus_scrape():
+    # GIVEN an empty config
+    cfg = Config()
+    # WHEN attempting to add scrape jobs without incoming metrics
+    cfg.add_prometheus_scrape([{"foo": "bar"}], incoming_metrics=False)
+    # THEN the prometheus receiver config is not added (or updated if existing config)
+    with pytest.raises(KeyError):
+        cfg._config["receivers"]["prometheus"]
+
+    # WHEN a scrape job is added to the config
+    first_job = [
+        {
+            "metrics_path": "/metrics",
+            "static_configs": [{"targets": ["*:9001"]}],
+            "job_name": "first_job",
+            "scrape_interval": "15s",
+        }
+    ]
+    expected_prom_recv_cfg = {
+        "config": {
+            "scrape_configs": [
+                {
+                    "metrics_path": "/metrics",
+                    "static_configs": [{"targets": ["*:9001"]}],
+                    "job_name": "first_job",
+                    "scrape_interval": "15s",
+                },
+            ]
+        }
+    }
+    cfg.add_prometheus_scrape(first_job, True)
+    # THEN it exists in the prometheus receiver config
+    assert cfg._config["receivers"]["prometheus"] == expected_prom_recv_cfg
+
+    # WHEN more scrape jobs are added to the config
+    more_jobs = [
+        {
+            "metrics_path": "/metrics",
+            "job_name": "second_job",
+        },
+        {
+            "metrics_path": "/metrics",
+            "job_name": "third_job",
+        },
+    ]
+    cfg.add_prometheus_scrape(more_jobs, True)
+    # THEN the original scrape job wasn't overwritten and the newly added scrape jobs were added
+    job_names = [
+        job["job_name"]
+        for job in cfg._config["receivers"]["prometheus"]["config"]["scrape_configs"]
+    ]
+    assert job_names == ["first_job", "second_job", "third_job"]
+
+
 def test_rendered_default_is_valid():
     # GIVEN a default config
     # WHEN the config is rendered
