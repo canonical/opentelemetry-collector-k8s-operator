b8e8a4894522f8c439a4bbf682f5fdd0e84afbc6
Jose C. Massón <jose.masson@canonical.com>
2025-07-01 15:27:47 -0300

    add missing endpoints to terraform output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,12 +5,18 @@ output "app_name" {
 output "endpoints" {
   value = {
     # Requires
-    metrics_endpoint  = "metrics-endpoint",
-    send_remote_write = "send_remote_write",
-    send_loki_logs    = "send-loki-logs",
-    receive_ca_cert   = "receive-ca-cert",
+    metrics_endpoint            = "metrics-endpoint",
+    send_remote_write           = "send_remote_write",
+    send_loki_logs              = "send-loki-logs",
+    receive_ca_cert             = "receive-ca-cert",
+    grafana_dashboards_consumer = "grafana-dashboards-consumer",
+    cloud_config                = "cloud-config",
+    receive_server_cert         = "receive-server-cert",
+    send_traces                 = "send-traces",
 
     # Provides
-    receive_loki_logs = "receive-loki-logs",
+    receive_loki_logs           = "receive-loki-logs",
+    grafana_dashboards_provider = "grafana-dashboards-provider",
+    receive_traces              = "receive-traces",
   }
 }
