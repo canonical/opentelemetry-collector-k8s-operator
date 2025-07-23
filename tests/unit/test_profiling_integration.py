68757990e210e16916454734fd4eb4a07a757885
Pietro Pasotti <pietro.pasotti@canonical.com>
2025-07-23 16:18:02 +0200

    completed unittest

--- tests/unit/test_profiling_integration.py
+++ tests/unit/test_profiling_integration.py
@@ -4,6 +4,7 @@ import pytest
 from ops.testing import Container, Relation, State
 
 from src.constants import SERVICE_NAME, CONFIG_PATH
+from tests.unit.helpers import get_otelcol_file
 
 
 @pytest.mark.parametrize("relation_joined", (True, False))
@@ -32,10 +33,11 @@ def test_profiling_integration(ctx, execs):
     # GIVEN otelcol deployed in isolation
     container = Container(name="otelcol", can_connect=True, execs=execs)
 
-    # WHEN a profiling relation joins but pyroscope didn't reply with an endpoint yet
+    pyro_url = "my.fqdn.cluster.local:12345"
+    # WHEN a profiling relation joins and pyroscope sent an endpoint
     profiling = Relation(
         endpoint="send-profiles",
-        remote_app_data={"otlp_grpc_endpoint_url": json.dumps("my.fqdn.cluster.local:12345")}
+        remote_app_data={"otlp_grpc_endpoint_url": json.dumps(pyro_url)}
     )
     state_in = State(relations=[profiling], containers=[container])
     state_out = ctx.run(ctx.on.update_status(), state=state_in)
@@ -44,3 +46,9 @@ def test_profiling_integration(ctx, execs):
     plan_out = state_out.get_container("otelcol").plan
     assert plan_out.services[SERVICE_NAME].command == f"/usr/bin/otelcol --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
     assert plan_out.checks["valid-config"].exec['command'] == f"otelcol validate --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
+
+    # AND the profiling pipeline contains an exporter to the expected url
+    cfg = get_otelcol_file(state_out, ctx, CONFIG_PATH)
+    assert cfg['service']['pipelines']['profiles']['exporters'][0] == 'profiling/0'
+    assert cfg['exporters']['profiling/0']['endpoint'] == pyro_url
+
