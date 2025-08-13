0fe7f08486168aa46e148efd6efc44cefe723798
Luca Bello <36242061+lucabello@users.noreply.github.com>
2025-08-13 16:51:31 +0200

    feat: add test for log ingestion via pebble (#104)
    * feat: add test for log ingestion via pebble
    
    * tox fmt
    
    * fix test
    
    * try to fix test
    
    * fetch-lib
    
    * revert fetch-lib
    
    * remove pytest-operator
    
    * remove async from charm
    
    * add dep to asyncio
    
    * fix things
    
    * fix things
    
    * add test_charm
    
    * fetch-lib
    
    * add blackbox exporter to wokeignore
    
    * revert timed memoizer
    
    ---------
    
    Signed-off-by: Luca Bello <36242061+lucabello@users.noreply.github.com>

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -8,29 +8,19 @@ Scenario: Standalone deployment
     Then all pebble checks pass
 """
 
-import sh
 from typing import Dict
-from pytest_operator.plugin import OpsTest
 
-# pyright: reportAttributeAccessIssue = false
+import jubilant
 
 
-def _get_pebble_checks(ops_test: OpsTest, app_name: str):
+def _get_pebble_checks(juju: jubilant.Juju, unit_name: str):
     """Get the pebble checks results."""
-    assert ops_test.model
-    return sh.juju.ssh(
-        f"--model={ops_test.model.name}",
-        "--container=otelcol",
-        f"{app_name}/leader",
-        "pebble checks",
-    )
+    return juju.ssh(unit_name, command="pebble checks", container="otelcol")
 
 
-async def test_pebble_checks(ops_test: OpsTest, charm: str, charm_resources: Dict[str, str]):
+def test_pebble_checks(juju: jubilant.Juju, charm: str, charm_resources: Dict[str, str]):
     """Deploy the charm."""
-    assert ops_test.model
-    app_name = "otel-collector-k8s"
-    await ops_test.model.deploy(charm, app_name, resources=charm_resources, trust=True)
-    await ops_test.model.wait_for_idle(apps=[app_name], status="active")
-    pebble_checks = _get_pebble_checks(ops_test=ops_test, app_name=app_name)
+    juju.deploy(charm, "otelcol", resources=charm_resources, trust=True)
+    juju.wait(jubilant.all_active)
+    pebble_checks = _get_pebble_checks(juju=juju, unit_name="otelcol/leader")
     assert "down" not in pebble_checks
