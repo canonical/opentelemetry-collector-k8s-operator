0e0085f0a180963c9b396bd105f6f756ee8cf6b2
Luca Bello <36242061+lucabello@users.noreply.github.com>
2025-09-04 11:22:41 +0200

    chore: align shared code with the machine charm (#122)
    * chore: align shared code with the machine charm
    
    * fix static
    
    * fix test name
    
    * fix scrape
    
    * tox fmt
    
    * fix scrape agaiN
    
    * fix integration tests
    
    * increase tracing timeout
    
    * chore: trigger ci
    
    * remove comment
    
    * Update src/config_builder.py
    
    Signed-off-by: Luca Bello <36242061+lucabello@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Luca Bello <36242061+lucabello@users.noreply.github.com>

--- tests/unit/test_config_manager.py
+++ tests/unit/test_config_manager.py
@@ -8,7 +8,12 @@ from config_manager import ConfigManager
 
 def test_add_prometheus_scrape():
     # GIVEN an empty config
-    config_manager = ConfigManager("", "", insecure_skip_verify=True)
+    config_manager = ConfigManager(
+        unit_name="fake/0",
+        global_scrape_interval="15s",
+        global_scrape_timeout="",
+        insecure_skip_verify=True,
+    )
 
     # WHEN a scrape job is added to the config
     first_job = [
@@ -36,7 +41,10 @@ def test_add_prometheus_scrape():
     config_manager.add_prometheus_scrape_jobs(first_job)
     # THEN it exists in the prometheus receiver config
     # AND insecure_skip_verify is injected into the config
-    assert config_manager.config._config["receivers"]["prometheus"] == expected_prom_recv_cfg
+    assert (
+        config_manager.config._config["receivers"]["prometheus/metrics-endpoint/fake/0"]
+        == expected_prom_recv_cfg
+    )
 
     # AND WHEN more scrape jobs are added to the config
     more_jobs = [
@@ -50,19 +58,24 @@ def test_add_prometheus_scrape():
         },
     ]
     config_manager.add_prometheus_scrape_jobs(more_jobs)
-    # THEN the original scrape job wasn't overwritten and the newly added scrape jobs were added
+    # THEN the original scrape job was overwritten and the newly added scrape jobs were added
     job_names = [
         job["job_name"]
-        for job in config_manager.config._config["receivers"]["prometheus"]["config"][
-            "scrape_configs"
-        ]
+        for job in config_manager.config._config["receivers"][
+            "prometheus/metrics-endpoint/fake/0"
+        ]["config"]["scrape_configs"]
     ]
-    assert job_names == ["first_job", "second_job", "third_job"]
+    assert job_names == ["second_job", "third_job"]
 
 
 def test_add_log_forwarding():
     # GIVEN an empty config
-    config_manager = ConfigManager("", "", insecure_skip_verify=True)
+    config_manager = ConfigManager(
+        unit_name="fake/0",
+        global_scrape_interval="",
+        global_scrape_timeout="",
+        insecure_skip_verify=True,
+    )
 
     # WHEN a loki exporter is added to the config
     expected_loki_forwarding_cfg = {
@@ -84,14 +97,21 @@ def test_add_log_forwarding():
         insecure_skip_verify=False,
     )
     # THEN it exists in the loki exporter config
-    config = dict(sorted(config_manager.config._config["exporters"]["loki/0"].items()))
+    config = dict(
+        sorted(config_manager.config._config["exporters"]["loki/send-loki-logs/0"].items())
+    )
     expected_config = dict(sorted(expected_loki_forwarding_cfg.items()))
     assert config == expected_config
 
 
 def test_add_traces_forwarding():
     # GIVEN an empty config
-    config_manager = ConfigManager("", "", insecure_skip_verify=True)
+    config_manager = ConfigManager(
+        unit_name="fake/0",
+        global_scrape_interval="",
+        global_scrape_timeout="",
+        insecure_skip_verify=True,
+    )
 
     # WHEN a traces exporter is added to the config
     expected_traces_forwarding_cfg = {
@@ -105,14 +125,21 @@ def test_add_traces_forwarding():
         endpoint="http://192.168.1.244:4318",
     )
     # THEN it exists in the traces exporter config
-    config = dict(sorted(config_manager.config._config["exporters"]["otlphttp/tempo"].items()))
+    config = dict(
+        sorted(config_manager.config._config["exporters"]["otlphttp/send-traces"].items())
+    )
     expected_config = dict(sorted(expected_traces_forwarding_cfg.items()))
     assert config == expected_config
 
 
 def test_add_remote_write():
     # GIVEN an empty config
-    config_manager = ConfigManager("", "", insecure_skip_verify=True)
+    config_manager = ConfigManager(
+        unit_name="fake/0",
+        global_scrape_interval="",
+        global_scrape_timeout="",
+        insecure_skip_verify=True,
+    )
 
     # WHEN a remote write exporter is added to the config
     expected_remote_write_cfg = {
@@ -126,7 +153,11 @@ def test_add_remote_write():
     )
     # THEN it exists in the remote write exporter config
     config = dict(
-        sorted(config_manager.config._config["exporters"]["prometheusremotewrite/0"].items())
+        sorted(
+            config_manager.config._config["exporters"][
+                "prometheusremotewrite/send-remote-write/0"
+            ].items()
+        )
     )
     expected_config = dict(sorted(expected_remote_write_cfg.items()))
     assert config == expected_config
--- tests/unit/test_profiling_integration.py
+++ tests/unit/test_profiling_integration.py
@@ -14,11 +14,15 @@ from tests.unit.helpers import get_otelcol_file
 
 @pytest.fixture
 def tls_mock(cert_obj, private_key):
-    with patch.object(
-        TLSCertificatesRequiresV4, "_find_available_certificates", return_value=None
-    ), patch.object(
-        TLSCertificatesRequiresV4, "get_assigned_certificate", return_value=(cert_obj, private_key)
-    ), patch.object(Certificate, "from_string", return_value=cert_obj):
+    with (
+        patch.object(TLSCertificatesRequiresV4, "_find_available_certificates", return_value=None),
+        patch.object(
+            TLSCertificatesRequiresV4,
+            "get_assigned_certificate",
+            return_value=(cert_obj, private_key),
+        ),
+        patch.object(Certificate, "from_string", return_value=cert_obj),
+    ):
         yield
 
 
@@ -30,9 +34,15 @@ def test_waiting_for_send_profiles_endpoint(ctx, execs, relation_joined):
 
     # WHEN a send_profiles relation joins but pyroscope didn't reply with an endpoint yet,
     # or the relation didn't join yet at all
-    relations = {Relation(
-        endpoint="send-profiles",
-    )} if relation_joined else {}
+    relations = (
+        {
+            Relation(
+                endpoint="send-profiles",
+            )
+        }
+        if relation_joined
+        else {}
+    )
 
     state_in = State(relations=relations, containers=[container])
     state_out = ctx.run(ctx.on.update_status(), state=state_in)
@@ -40,7 +50,10 @@ def test_waiting_for_send_profiles_endpoint(ctx, execs, relation_joined):
     # THEN the pebble layer command and check contain no feature gate
     plan_out = state_out.get_container("otelcol").plan
     assert plan_out.services[SERVICE_NAME].command == f"/usr/bin/otelcol --config={CONFIG_PATH}"
-    assert plan_out.checks["valid-config"].exec['command'] == f"otelcol validate --config={CONFIG_PATH}"
+    assert (
+        plan_out.checks["valid-config"].exec["command"]
+        == f"otelcol validate --config={CONFIG_PATH}"
+    )
 
 
 @pytest.mark.parametrize("relation_joined", (True, False))
@@ -50,15 +63,26 @@ def test_waiting_for_receive_profiles_endpoint(ctx, execs, relation_joined):
     container = Container(name="otelcol", can_connect=True, execs=execs)
 
     # WHEN a receive_profiles relation joins
-    state_in = State(relations={Relation(
-        endpoint="receive-profiles",
-    )}, containers=[container])
+    state_in = State(
+        relations={
+            Relation(
+                endpoint="receive-profiles",
+            )
+        },
+        containers=[container],
+    )
     state_out = ctx.run(ctx.on.update_status(), state=state_in)
 
     # THEN the pebble layer command and check contain the feature gate
     plan_out = state_out.get_container("otelcol").plan
-    assert plan_out.services[SERVICE_NAME].command == f"/usr/bin/otelcol --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
-    assert plan_out.checks["valid-config"].exec['command'] == f"otelcol validate --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
+    assert (
+        plan_out.services[SERVICE_NAME].command
+        == f"/usr/bin/otelcol --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
+    )
+    assert (
+        plan_out.checks["valid-config"].exec["command"]
+        == f"otelcol validate --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
+    )
 
 
 @pytest.mark.parametrize("insecure_skip_verify", (True, False))
@@ -75,23 +99,35 @@ def test_send_profiles_integration(ctx, execs, insecure_skip_verify, remote_inse
         remote_app_data={
             "otlp_grpc_endpoint_url": json.dumps(pyro_url),
             "insecure": json.dumps(remote_insecure),
-        }
+        },
     )
-    state_in = State(relations=[send_profiles], containers=[container],
-                     config={"tls_insecure_skip_verify": insecure_skip_verify})
+    state_in = State(
+        relations=[send_profiles],
+        containers=[container],
+        config={"tls_insecure_skip_verify": insecure_skip_verify},
+    )
     state_out = ctx.run(ctx.on.update_status(), state=state_in)
 
     # THEN the pebble layer command and check contain the profilesSupport feature gate
     plan_out = state_out.get_container("otelcol").plan
-    assert plan_out.services[SERVICE_NAME].command == f"/usr/bin/otelcol --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
-    assert plan_out.checks["valid-config"].exec['command'] == f"otelcol validate --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
+    assert (
+        plan_out.services[SERVICE_NAME].command
+        == f"/usr/bin/otelcol --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
+    )
+    assert (
+        plan_out.checks["valid-config"].exec["command"]
+        == f"otelcol validate --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
+    )
 
     # AND the profiling pipeline contains an exporter to the expected url
     cfg = get_otelcol_file(state_out, ctx, CONFIG_PATH)
-    assert cfg['service']['pipelines']['profiles']['exporters'][0] == 'otlp/profiling/0'
-    assert cfg['service']['pipelines']['profiles']['receivers'][0] == "otlp"
-    assert cfg['exporters']['otlp/profiling/0']['endpoint'] == pyro_url
-    assert cfg["exporters"]["otlp/profiling/0"]["tls"] == {"insecure": remote_insecure, "insecure_skip_verify": insecure_skip_verify}
+    assert cfg["service"]["pipelines"]["profiles"]["exporters"][0] == "otlp/profiling/0"
+    assert cfg["service"]["pipelines"]["profiles"]["receivers"][0] == "otlp"
+    assert cfg["exporters"]["otlp/profiling/0"]["endpoint"] == pyro_url
+    assert cfg["exporters"]["otlp/profiling/0"]["tls"] == {
+        "insecure": remote_insecure,
+        "insecure_skip_verify": insecure_skip_verify,
+    }
 
 
 @patch("socket.getfqdn", return_value="localhost")
@@ -102,26 +138,35 @@ def test_receive_profiles_integration(sock_mock, ctx, execs, insecure_skip_verif
     container = Container(name="otelcol", can_connect=True, execs=execs)
 
     # WHEN a receive-profiles relation joins and pyroscope sent an endpoint
-    receive_profiles = Relation(
-        endpoint="receive-profiles"
+    receive_profiles = Relation(endpoint="receive-profiles")
+    state_in = State(
+        relations=[receive_profiles],
+        containers=[container],
+        config={"tls_insecure_skip_verify": insecure_skip_verify},
+        leader=True,
     )
-    state_in = State(relations=[receive_profiles], containers=[container],
-                     config={"tls_insecure_skip_verify": insecure_skip_verify},
-                     leader=True)
     state_out = ctx.run(ctx.on.update_status(), state=state_in)
 
     # THEN the pebble layer command and check contain the profilesSupport feature gate
     plan_out = state_out.get_container("otelcol").plan
-    assert plan_out.services[SERVICE_NAME].command == f"/usr/bin/otelcol --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
-    assert plan_out.checks["valid-config"].exec['command'] == f"otelcol validate --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
+    assert (
+        plan_out.services[SERVICE_NAME].command
+        == f"/usr/bin/otelcol --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
+    )
+    assert (
+        plan_out.checks["valid-config"].exec["command"]
+        == f"otelcol validate --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
+    )
 
     # AND the profiling pipeline contains a profiling pipeline, but no exporters other than debug
     cfg = get_otelcol_file(state_out, ctx, CONFIG_PATH)
-    assert cfg['service']['pipelines']['profiles']['exporters'] == ['debug']
+    assert cfg["service"]["pipelines"]["profiles"]["exporters"] == [
+        "debug/opentelemetry-collector-k8s/0"
+    ]
 
     # AND we publish to app databag our profile ingestion endpoints for otlp_grpc
     receive_profiles_app_data = state_out.get_relation(receive_profiles.id).local_app_data
-    assert  receive_profiles_app_data['otlp_grpc_endpoint_url']
+    assert receive_profiles_app_data["otlp_grpc_endpoint_url"]
 
 
 @pytest.mark.parametrize("insecure_skip_verify", (True, False))
@@ -141,21 +186,31 @@ def test_profiling_integration_tls(ctx, execs, insecure_skip_verify, tls_mock):
         endpoint="send-profiles",
         remote_app_data={
             "otlp_grpc_endpoint_url": json.dumps(pyro_url),
-        }
+        },
     )
-    state_in = State(relations=[profiling, ssc], containers=[container],
-                     config={"tls_insecure_skip_verify": insecure_skip_verify})
+    state_in = State(
+        relations=[profiling, ssc],
+        containers=[container],
+        config={"tls_insecure_skip_verify": insecure_skip_verify},
+    )
     state_out = ctx.run(ctx.on.update_status(), state=state_in)
 
     # THEN the pebble layer command and check contain the profilesSupport feature gate
     plan_out = state_out.get_container("otelcol").plan
-    assert plan_out.services[SERVICE_NAME].command == f"/usr/bin/otelcol --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
-    assert plan_out.checks["valid-config"].exec['command'] == f"otelcol validate --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
+    assert (
+        plan_out.services[SERVICE_NAME].command
+        == f"/usr/bin/otelcol --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
+    )
+    assert (
+        plan_out.checks["valid-config"].exec["command"]
+        == f"otelcol validate --config={CONFIG_PATH} --feature-gates=service.profilesSupport"
+    )
 
     # AND the profiling pipeline contains an exporter to the expected url
     cfg = get_otelcol_file(state_out, ctx, CONFIG_PATH)
-    assert cfg['service']['pipelines']['profiles']['exporters'][0] == 'otlp/profiling/0'
-    assert cfg['exporters']['otlp/profiling/0']['endpoint'] == pyro_url
-    assert cfg["exporters"]["otlp/profiling/0"]["tls"] == {"insecure": False, "insecure_skip_verify": insecure_skip_verify}
-
-
+    assert cfg["service"]["pipelines"]["profiles"]["exporters"][0] == "otlp/profiling/0"
+    assert cfg["exporters"]["otlp/profiling/0"]["endpoint"] == pyro_url
+    assert cfg["exporters"]["otlp/profiling/0"]["tls"] == {
+        "insecure": False,
+        "insecure_skip_verify": insecure_skip_verify,
+    }
