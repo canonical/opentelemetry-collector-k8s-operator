a2f5333f85c23aa96d00341cc8d3cdcf3119795c
Jose C. Massón <939888+Abuelodelanada@users.noreply.github.com>
2025-11-24 15:54:49 -0300

    Update src/config_manager.py
    Co-authored-by: Michael Thamm <mike.thamm@canonical.com>
    Signed-off-by: Jose C. Massón <939888+Abuelodelanada@users.noreply.github.com>

--- src/config_manager.py
+++ src/config_manager.py
@@ -587,7 +587,8 @@ class ConfigManager:
             if job_name in cert_paths:
                 tls_config = job.get("tls_config", {})
                 tls_config["ca_file"] = cert_paths[job_name]
-                tls_config.pop("ca") if "ca" in tls_config else None
+                if "ca" in tls_config:
+                	tls_config.pop("ca")
                 job["tls_config"] = tls_config
                 logger.debug(f"updated job '{job_name}' to use certificate path: {cert_paths[job_name]}")
 
