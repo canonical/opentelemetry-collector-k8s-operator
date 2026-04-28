8c77406ad9d3b4e8bae8bb7214a0dd05a6cdd4cf
Michael Thamm <mike.thamm@canonical.com>
2026-04-28 14:33:49 -0400

    feat: TF ingress offer

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,26 +4,27 @@ output "app_name" {
 
 output "provides" {
   value = {
-    receive_loki_logs           = "receive-loki-logs",
     grafana_dashboards_provider = "grafana-dashboards-provider",
-    receive_traces              = "receive-traces",
-    receive_otlp                = "receive-otlp",
     provide_cmr_mesh            = "provide-cmr-mesh",
+    receive_loki_logs           = "receive-loki-logs",
+    receive_otlp                = "receive-otlp",
+    receive_traces              = "receive-traces",
   }
 }
 
 output "requires" {
   value = {
-    metrics_endpoint            = "metrics-endpoint",
-    send_remote_write           = "send-remote-write",
-    send_loki_logs              = "send-loki-logs",
-    receive_ca_cert             = "receive-ca-cert",
-    grafana_dashboards_consumer = "grafana-dashboards-consumer",
     cloud_config                = "cloud-config",
+    metrics_endpoint            = "metrics-endpoint",
+    grafana_dashboards_consumer = "grafana-dashboards-consumer",
+    ingress                     = "ingress",
+    receive_ca_cert             = "receive-ca-cert",
     receive_server_cert         = "receive-server-cert",
-    send_traces                 = "send-traces",
-    send_otlp                   = "send-otlp",
-    service_mesh                = "service-mesh",
     require_cmr_mesh            = "require-cmr-mesh",
+    send_loki_logs              = "send-loki-logs",
+    send_otlp                   = "send-otlp",
+    send_remote_write           = "send-remote-write",
+    send_traces                 = "send-traces",
+    service_mesh                = "service-mesh",
   }
 }
