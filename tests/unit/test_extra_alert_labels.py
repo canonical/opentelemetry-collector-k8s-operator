8979eda6f64838b0c24fb1ee99bef6bdffe34723
Michael Thamm <mike.thamm@canonical.com>
2026-04-30 13:19:09 -0400

    chore

--- tests/unit/test_extra_alert_labels.py
+++ tests/unit/test_extra_alert_labels.py
@@ -2,11 +2,28 @@
 # See LICENSE file for licensing details.
 
 import json
-from typing import Dict, Union
+from typing import Any, Dict, Union
 
-from ops.testing import Relation, State
+from cosl.utils import LZMABase64
+from ops.testing import Relation, State, Model
 
 ConfigDict = Dict[str, Union[str, int, float, bool]]
+
+MODEL_NAME = "my_model"
+MODEL_UUID = "74a5690b-89c9-44dd-984b-f69f26a6b751"
+MODEL = Model(MODEL_NAME, uuid=MODEL_UUID)
+ZINC_TOPOLOGY_LABELS = {
+    "juju_application": "zinc",
+    "juju_charm": "zinc-k8s",
+    "juju_model": MODEL_NAME,
+    "juju_model_uuid": MODEL_UUID,
+}
+OTLP_TOPOLOGY_LABELS = {
+    "juju_application": "opentelemetry-collector-k8s",
+    "juju_charm": "opentelemetry-collector-k8s",
+    "juju_model": MODEL_NAME,
+    "juju_model_uuid": MODEL_UUID,
+}
 
 zinc_alerts = {
     "alert_rules": json.dumps(
@@ -20,8 +37,8 @@ zinc_alerts = {
                             "expr": "up == 0",
                             "for": "0m",
                             "labels": {
-                                "juju_model": "my_model",
-                                "juju_model_uuid": "74a5690b-89c9-44dd-984b-f69f26a6b751",
+                                "juju_model": MODEL_NAME,
+                                "juju_model_uuid": MODEL_UUID,
                                 "juju_application": "zinc",
                                 "juju_charm": "zinc-k8s",
                             },
@@ -34,9 +51,46 @@ zinc_alerts = {
 }
 
 
-def test_extra_alerts_config(ctx, otelcol_container):
+def _assert_extra_labels_present(
+    alert_rules: Dict[str, Any],
+    extra_labels: Dict[str, str],
+    databag_group_substr: str = "_alertgroup_",  # from Zinc rules group name
+    databag_labels: Dict[str, str] = ZINC_TOPOLOGY_LABELS,
+    default_labels: Dict[str, str] = OTLP_TOPOLOGY_LABELS,
+):
+    """Assert that all rules contain the expected extra alert labels and topology labels."""
+    assert alert_rules.get("groups"), "No groups found in alert rules"
+    for group in alert_rules["groups"]:
+        common_labels = databag_labels if databag_group_substr in group["name"] else default_labels
+        for rule in group["rules"]:
+            for key, value in extra_labels.items():
+                assert rule["labels"][key] == value
+            for key, value in common_labels.items():
+                assert rule["labels"][key] == value
+
+
+def _assert_extra_labels_absent(
+    alert_rules: Dict[str, Any],
+    extra_labels: Dict[str, str],
+    databag_group_substr: str = "_alertgroup_",  # from Zinc rules group name
+    databag_labels: Dict[str, str] = ZINC_TOPOLOGY_LABELS,
+    default_labels: Dict[str, str] = OTLP_TOPOLOGY_LABELS,
+):
+    """Assert that none of the rules contain the extra alert labels, but topology is preserved."""
+    assert alert_rules.get("groups"), "No groups found in alert rules"
+    for group in alert_rules["groups"]:
+        common_labels = databag_labels if databag_group_substr in group["name"] else default_labels
+        for rule in group["rules"]:
+            for key in extra_labels:
+                assert key not in rule["labels"]
+            for key, value in common_labels.items():
+                assert rule["labels"][key] == value
+
+
+def test_extra_metrics_alerts_config(ctx, otelcol_container):
     # GIVEN a new key-value pair of extra alerts labels, for instance:
     # juju config otelcol extra_alerts_labels="environment: PRODUCTION, zone=Mars"
+    extra_labels = {"environment": "PRODUCTION", "zone": "Mars"}
     config1: ConfigDict = {
         "extra_alert_labels": "environment: PRODUCTION, zone=Mars",
     }
@@ -48,10 +102,8 @@ def test_extra_alerts_config(ctx, otelcol_container):
     remote_write_relation = Relation("send-remote-write", remote_app_name="prometheus")
     state = State(
         leader=True,
-        relations=[
-            metrics_endpoint_relation,
-            remote_write_relation,
-        ],
+        model=MODEL,
+        relations=[metrics_endpoint_relation, remote_write_relation],
         containers=otelcol_container,
         config=config1,  # type: ignore
     )
@@ -62,16 +114,7 @@ def test_extra_alerts_config(ctx, otelcol_container):
     alert_rules = json.loads(
         out_1.get_relation(remote_write_relation.id).local_app_data["alert_rules"]
     )
-
-    for group in alert_rules["groups"]:
-        for rule in group["rules"]:
-            assert rule["labels"]["environment"] == "PRODUCTION"
-            assert rule["labels"]["zone"] == "Mars"
-            if "opentelemetry_collector_k8s_alertgroup_alerts" in group["name"]:
-                assert rule["labels"]["juju_application"] == "zinc"
-                assert rule["labels"]["juju_charm"] == "zinc-k8s"
-                assert rule["labels"]["juju_model"] == "my_model"
-                assert rule["labels"]["juju_model_uuid"] == "74a5690b-89c9-44dd-984b-f69f26a6b751"
+    _assert_extra_labels_present(alert_rules, extra_labels)
 
     # GIVEN the config option for extra alert labels is unset
     config2: ConfigDict = {"extra_alert_labels": ""}
@@ -79,6 +122,7 @@ def test_extra_alerts_config(ctx, otelcol_container):
     # THEN the only labels present in the alert are the JujuTopology labels
     next_state = State(
         leader=True,
+        model=MODEL,
         relations=out_1.relations,
         containers=out_1.containers,
         config=config2,
@@ -87,24 +131,15 @@ def test_extra_alerts_config(ctx, otelcol_container):
     alert_rules_mod = json.loads(
         out_2.get_relation(remote_write_relation.id).local_app_data["alert_rules"]
     )
-
-    for group in alert_rules_mod["groups"]:
-        for rule in group["rules"]:
-            assert "environment" not in rule["labels"].keys()
-            assert "zone" not in rule["labels"].keys()
-            if "opentelemetry_collector_k8s_alertgroup_alerts" in group["name"]:
-                assert rule["labels"]["juju_application"] == "zinc"
-                assert rule["labels"]["juju_charm"] == "zinc-k8s"
-                assert rule["labels"]["juju_model"] == "my_model"
-                assert rule["labels"]["juju_model_uuid"] == "74a5690b-89c9-44dd-984b-f69f26a6b751"
+    _assert_extra_labels_absent(alert_rules_mod, extra_labels)
 
 
+# TODO: I am getting CosTool validation errors in the logs, check to see if this happens on main as well
 def test_extra_loki_alerts_config(ctx, otelcol_container):
     # GIVEN a new key-value pair of extra alerts labels, for instance:
     # juju config otelcol extra_alerts_labels="environment: PRODUCTION, zone=Mars"
-    config1: ConfigDict = {
-        "extra_alert_labels": "environment: PRODUCTION, zone=Mars",
-    }
+    extra_labels = {"environment": "PRODUCTION", "zone": "Mars"}
+    config1: ConfigDict = {"extra_alert_labels": "environment: PRODUCTION, zone=Mars"}
 
     # THEN The extra_alert_labels MUST be added to the alert rules.
     receive_loki_logs_relation = Relation(
@@ -113,10 +148,8 @@ def test_extra_loki_alerts_config(ctx, otelcol_container):
     send_loki_logs_relation = Relation("send-loki-logs", remote_app_name="loki")
     state = State(
         leader=True,
-        relations=[
-            receive_loki_logs_relation,
-            send_loki_logs_relation,
-        ],
+        model=MODEL,
+        relations=[receive_loki_logs_relation, send_loki_logs_relation],
         containers=otelcol_container,
         config=config1,  # type: ignore
     )
@@ -127,16 +160,7 @@ def test_extra_loki_alerts_config(ctx, otelcol_container):
     alert_rules = json.loads(
         out_1.get_relation(send_loki_logs_relation.id).local_app_data["alert_rules"]
     )
-
-    for group in alert_rules["groups"]:
-        for rule in group["rules"]:
-            assert rule["labels"]["environment"] == "PRODUCTION"
-            assert rule["labels"]["zone"] == "Mars"
-            if "opentelemetry_collector_k8s_alertgroup_alerts" in group["name"]:
-                assert rule["labels"]["juju_application"] == "zinc"
-                assert rule["labels"]["juju_charm"] == "zinc-k8s"
-                assert rule["labels"]["juju_model"] == "my_model"
-                assert rule["labels"]["juju_model_uuid"] == "74a5690b-89c9-44dd-984b-f69f26a6b751"
+    _assert_extra_labels_present(alert_rules, extra_labels)
 
     # GIVEN the config option for extra alert labels is unset
     config2: ConfigDict = {"extra_alert_labels": ""}
@@ -144,6 +168,7 @@ def test_extra_loki_alerts_config(ctx, otelcol_container):
     # THEN the only labels present in the alert are the JujuTopology labels
     next_state = State(
         leader=True,
+        model=MODEL,
         relations=out_1.relations,
         containers=out_1.containers,
         config=config2,
@@ -152,13 +177,52 @@ def test_extra_loki_alerts_config(ctx, otelcol_container):
     alert_rules_mod = json.loads(
         out_2.get_relation(send_loki_logs_relation.id).local_app_data["alert_rules"]
     )
+    _assert_extra_labels_absent(alert_rules_mod, extra_labels)
 
-    for group in alert_rules_mod["groups"]:
-        for rule in group["rules"]:
-            assert "environment" not in rule["labels"].keys()
-            assert "zone" not in rule["labels"].keys()
-            if "opentelemetry_collector_k8s_alertgroup_alerts" in group["name"]:
-                assert rule["labels"]["juju_application"] == "zinc"
-                assert rule["labels"]["juju_charm"] == "zinc-k8s"
-                assert rule["labels"]["juju_model"] == "my_model"
-                assert rule["labels"]["juju_model_uuid"] == "74a5690b-89c9-44dd-984b-f69f26a6b751"
+
+def test_extra_otlp_alerts_config(ctx, otelcol_container, all_rules):
+    # GIVEN a new key-value pair of extra alerts labels
+    extra_labels = {"environment": "PRODUCTION", "zone": "Mars"}
+    config1: ConfigDict = {"extra_alert_labels": "environment: PRODUCTION, zone=Mars"}
+
+    # THEN The extra_alert_labels MUST be added to the alert rules in the send-otlp databag.
+    receive_otlp_relation = Relation(
+        "receive-otlp",
+        remote_app_data={"rules": json.dumps(all_rules, sort_keys=True), "metadata": "{}"},
+    )
+    send_otlp_relation = Relation("send-otlp")
+    state = State(
+        leader=True,
+        model=MODEL,
+        relations=[receive_otlp_relation, send_otlp_relation],
+        containers=otelcol_container,
+        config=config1,  # type: ignore
+    )
+    out_0 = ctx.run(ctx.on.relation_changed(relation=receive_otlp_relation), state)
+    out_1 = ctx.run(
+        ctx.on.relation_joined(relation=out_0.get_relation(send_otlp_relation.id)), out_0
+    )
+    compressed_rules = out_1.get_relation(send_otlp_relation.id).local_app_data.get("rules")
+    assert compressed_rules
+    decompressed = json.loads(LZMABase64.decompress(json.loads(compressed_rules)))
+    # Check promql rules have extra labels
+    for groups in decompressed.get("logql", {}), decompressed.get("promql", {}):
+        _assert_extra_labels_present(groups, extra_labels)
+
+    # GIVEN the config option for extra alert labels is unset
+    config2: ConfigDict = {"extra_alert_labels": ""}
+
+    # THEN the extra labels are no longer present
+    next_state = State(
+        leader=True,
+        model=MODEL,
+        relations=out_1.relations,
+        containers=out_1.containers,
+        config=config2,
+    )
+    out_2 = ctx.run(ctx.on.config_changed(), next_state)
+    compressed_rules_mod = out_2.get_relation(send_otlp_relation.id).local_app_data.get("rules")
+    assert compressed_rules_mod
+    decompressed_mod = json.loads(LZMABase64.decompress(json.loads(compressed_rules_mod)))
+    for groups in decompressed_mod.get("logql", {}), decompressed_mod.get("promql", {}):
+        _assert_extra_labels_absent(groups, extra_labels)
