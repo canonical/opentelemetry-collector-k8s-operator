bf03a324ef3f9501c3ac5e4807757f39157ed3f2
Luca Bello <luca.bello@canonical.com>
2026-08-26 10:05:07 +0200

    chore: migrate certificate-transfer charm lib to charmlibs package
    Replaces the store-fetched certificate_transfer_interface charm lib with the charmlibs-interfaces-certificate_transfer PyPI package, matching the approach already used for otlp and pathops.

--- src/integrations.py
+++ src/integrations.py
@@ -15,7 +15,7 @@ from urllib.parse import urlparse
 import yaml
 from charmlibs.interfaces.otlp import OtlpEndpoint, OtlpProvider, OtlpRequirer, RuleStore
 from charmlibs.pathops import PathProtocol
-from charms.certificate_transfer_interface.v1.certificate_transfer import (
+from charmlibs.interfaces.certificate_transfer import (
     CertificateTransferRequires,
 )
 from charms.grafana_cloud_integrator.v0.cloud_config_requirer import (
@@ -36,7 +36,9 @@ from charms.istio_ingress_k8s.v0.istio_ingress_route import (
     ProtocolType,
 )
 from charms.loki_k8s.v1.loki_push_api import LokiPushApiConsumer, LokiPushApiProvider
-from charms.opentelemetry_collector_integrator.v0.opentelemetry_collector_integrator import OtelcolIntegratorRequirer
+from charms.opentelemetry_collector_integrator.v0.opentelemetry_collector_integrator import (
+    OtelcolIntegratorRequirer,
+)
 from charms.prometheus_k8s.v0.prometheus_scrape import (
     MetricsEndpointConsumer,
 )
